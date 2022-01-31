9bb799a30f301fcae1dc050af767e7de10e50979
anversa-pro <angelavergaras@gmail.com>
2022-01-31 17:36:10 +0000

    [MODIFY] delete spaces and modify comments format

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application listening 0.0.0.0 port 5000:
-    The route of the domain / displays 'Hello HBNB!'
+""" 
+Script that starts a Flask web application listening 0.0.0.0 port 5000:
+The route of the domain / displays 'Hello HBNB!'
 """
-
 from flask import Flask
 import sys
 
@@ -11,19 +11,25 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def domainFunction():
-    """ Responsible of the domain route, displays a message """
+    """ 
+    Responsible of the domain route, displays a message 
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnbFunction():
-    """ Responsible of the /hbhb route, displays a message """
+    """
+    Responsible of the /hbhb route, displays a message 
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def textFunction(text):
-    """ Responsible of the /C/text route, displays text """
+    """ 
+    Responsible of the /C/text route, displays text
+    """
     text = text.replace('_', ' ')
     return f'C {text}'
 
