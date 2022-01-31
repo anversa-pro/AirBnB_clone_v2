81f779ade52e6f4057272fa87c780852ff9b57e9
anversa-pro <angelavergaras@gmail.com>
2022-01-31 16:22:02 +0000

    [CREATE] message for a specific route and a int variable

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application listening 0.0.0.0 port 5000:
+    The route of the domain / displays 'Hello HBNB!'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def domainFunction():
+    """ Responsible of the domain route, displays a message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnbFunction():
+    """ Responsible of the /hbhb route, displays a message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def textCFunction(text):
+    """ Responsible of the /C/text route, displays text """
+    text = text.replace('_', ' ')
+    return f'C {text}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def textPFunction(text='is cool'):
+    """ Responsible of the /python/text route, displays text """
+    text = text.replace('_', ' ')
+    return f'Python {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numberFunction(n):
+    """ Responsible of the /python/text route, displays text """
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
